77ab327c26a87a9280edfff66e99a65581bb0813
raphael <rafael.dks10@gmail.com>
2025-05-27 18:15:30 -0300

    templates

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -24,7 +24,7 @@ def register():
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (name, password) VALUES (?, ?)",
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
                     (username, generate_password_hash(password))
                 )
                 db.commit()
