502a87443171604ce7f502c35114ae51651900ff
raphael <rafael.dks10@gmail.com>
2025-05-28 22:33:48 -0300

    fixing create, update and delete

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -3,10 +3,34 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
-from flaskr.db import get_db
+from .db import get_db
 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
@@ -65,30 +89,7 @@ def login():
     return render_template('auth/login.html')
 
 
-@bp.before_app_request
-def load_logged_in_user():
-    user_id = session.get('user_id')
-
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone
-
-
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
-
-
-def login_required(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login'))
-
-        return view(**kwargs)
-
-    return wrapped_view
--- flaskr/blog.py
+++ flaskr/blog.py
@@ -2,7 +2,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
-from flaskr.auth import login_required, load_logged_in_user
+from flaskr.auth import login_required
 from flaskr.db import get_db
 
 
@@ -14,7 +14,7 @@ def get_post(id, check_author=True):
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
-        (id)
+        (id,)
     ).fetchone()
 
     if post is None:
@@ -53,8 +53,7 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
+                'INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)',
                 (title, body, g.user['id'])
             )
             db.commit()
@@ -96,6 +95,6 @@ def update(id):
 def delete(id):
     get_post(id)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id))
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
