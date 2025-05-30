e2f4c7b973f63a37625c6630bb44fc2721faca02
raphael <rafael.dks10@gmail.com>
2025-05-30 07:01:27 -0300

    authentication test

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,20 @@ with open(os.path.join(__file__), 'data.sql', 'rb') as f:
     _data_sql = f.read().decode('utf-8')
 
 
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+    
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
 @pytest.fixture
 def app():
     db_fd, db_path = tempfile.mkstemp()
@@ -35,3 +49,8 @@ def client(app):
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
+
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
